09e9d19c9ca32572f56dde5d65d922d4b403b9b1
yixinduang <aliceduan2017@gmail.com>
2023-03-01 19:53:36 -0800

    adjust project layout

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-
-
+// for the nav part
+const act = document.getElementsByClassName("active");
+function active(ele) {
+  console.log(act);
+  act[0].className = "unactive";
+  ele.className = "active";
+}
